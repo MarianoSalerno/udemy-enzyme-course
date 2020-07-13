86a638e232fb8827a6de1988f32f42abf7d29b19
Mariano Salerno <mariano.salerno@toptal.com>
2020-07-13 17:42:48 -0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
+import GuessedWords from './GuessedWords'
+import Congrats from './Congrats'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      counter: 0
-    }
-  }
   render() {
     return (
-      <div data-test='component-app' >
-        <h1 data-test='counter-display'> The counter is currently {this.state.counter} </h1>
-        <button 
-          data-test='increment-button'
-          onClick= {() => this.setState({counter: this.state.counter + 1})}>
-            Increment counter
-        </button>
+      <div className='container'>
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[ {guessedWord: 'train', letterMatchCount: 3 } ]}/>
       </div>
     )
   }
