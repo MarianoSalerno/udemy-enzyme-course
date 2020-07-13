e7b46debd5be776df63b1f991523976ff9f86aed
Mariano Salerno <mariano.salerno@toptal.com>
2020-07-13 15:18:08 -0300

    added prop types

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,28 +2,40 @@ import React from 'react'
 import Congrats from './Congrats'
 import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
-import { findByTestAttr } from '../test/testUtils'
+import { findByTestAttr, checkProps } from '../test/testUtils'
 
 Enzyme.configure({adapter: new EnzymeAdapter()})
 
+const defaultProps = { success: false }
+
 const setup = (props={}) => {
-  return shallow(<Congrats {...props}/>)
+  const setupProps = { ...defaultProps, ...props}
+  return shallow(<Congrats {...setupProps}/>)
 }
 
 test('renders without error', () => {
   const wrapper = setup()
   const component = findByTestAttr(wrapper, 'component-congrats')
+  
   expect(component.length).toBe(1)
 })
 
 test('doesnt render when prop success is false', () => {
-  const wrapper = setup({success: false})
+  const wrapper = setup({ success: false })
   const component = findByTestAttr(wrapper, 'component-congrats')
+  
   expect(component.text()).toBe('')
 })
 
 test('renders message when prop success is true', () => {
   const wrapper = setup({success: true})
   const message = findByTestAttr(wrapper, 'congrats-message')
+  
   expect(message.text().length).not.toBe(0)
+})
+
+test('does not throw warning with expected props', () => {
+  const expectedProps = {success: false}
+  
+  checkProps(Congrats, expectedProps)
 })
