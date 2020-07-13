b956eb0413477b422ca201ea681e398b693e3de9
Mariano Salerno <mariano.salerno@toptal.com>
2020-07-13 20:24:44 -0300

    App component with hooks

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import Congrats from './Congrats'
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords guessedWords={[ {guessedWord: 'train', letterMatchCount: 3 } ]}/>
+      <div className='container' data-test='component-app'>
       </div>
     )
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { shallow } from 'enzyme'
+import { findByTestAttr } from '../test/testUtils'
 import App from './App'
-import Enzyme, { shallow } from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
 
-Enzyme.configure({adapter: new EnzymeAdapter()})
+const setup = () => {
+  return shallow(<App />)
+}
 
-test('renders without error', () => {
-
+test('App renders without error', () => {
+  const wrapper = setup()
+  const appComponent = findByTestAttr(wrapper, 'component-app')
+  expect(appComponent.length).toBe(1)
 })
