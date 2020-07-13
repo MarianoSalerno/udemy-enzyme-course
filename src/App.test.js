be5f6476359e9fea52b18e69280a8d162226d924
Mariano Salerno <mariano.salerno@toptal.com>
2020-07-13 12:19:15 -0300

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import App from './App'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({adapter: new EnzymeAdapter()})
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />)
+  const appComponent = wrapper.find('[data-test="component-app"]')
+  expect(appComponent.length).toBe(1)
+})
+
+test('renders increment button', () => {
+  const wrapper = shallow(<App />)
+})
+
+test('renders display counter', () => {
+  const wrapper = shallow(<App />)
+})
+
+test('counter starts at 0', () => {
+  const wrapper = shallow(<App />)
+})
+
+test('clicking button increments counter', () => {
+  const wrapper = shallow(<App />)
+})
