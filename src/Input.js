8bf3bf716c69a328e629207d31bcd119a310aa38
Mariano Salerno <mariano.salerno@toptal.com>
2020-07-18 13:25:30 -0300

    currentGuess state code

--- src/Input.js
+++ src/Input.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-function Input(props) {
+function Input({ secretWord }) {
+  const [ currentGuess, setCurrentState ] = React.useState("")
   return (
     <div data-test='component-input'>
+      <form className="form-inline">
+        <input 
+          data-test="input-box" 
+          className="mb-2 mx-sm-3" 
+          type="text" 
+          placeholder="Enter guess" value={currentGuess} 
+          onChange={(event) => setCurrentState(event.target.value)} />
+        <button 
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          onClick={(evt) => {evt.preventDefault()}}>
+          Submit
+        </button>
+      </form>
     </div>
   )
+}
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired
 }
 
 export default Input;
