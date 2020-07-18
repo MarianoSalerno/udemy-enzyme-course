dbe2c7c5aede5f64f139f4a6acbdb8f70ea40881
Mariano Salerno <mariano.salerno@toptal.com>
2020-07-18 14:32:08 -0300

    set secret word on app mount

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { findByTestAttr } from '../test/testUtils'
 import App from './App'
+import hookActions from './actions/hookActions'
+
+const mockGetSecretWord = jest.fn()
 
 const setup = () => {
-  return shallow(<App />)
+  mockGetSecretWord.mockClear()
+  hookActions.getSecretWord = mockGetSecretWord
+  return mount(<App />)
 }
 
 test('App renders without error', () => {
   const wrapper = setup()
   const appComponent = findByTestAttr(wrapper, 'component-app')
   expect(appComponent.length).toBe(1)
+})
+
+describe('getSecretWord calls', () => {
+  test('gets called on App mount', () => {
+    setup()
+
+    expect(mockGetSecretWord).toHaveBeenCalled()
+  })
 })
