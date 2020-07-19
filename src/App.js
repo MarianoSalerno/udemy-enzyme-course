6260dcdef2cba940692ff9b96fc90bf52a605174
Mariano Salerno <mariano.salerno@toptal.com>
2020-07-19 11:59:19 -0300

    language context in congrats component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React from 'react';
 import './App.css';
 import hookActions from './actions/hookActions';
 import Input from './Input'
+import LanguageContext from './contexts/languageContext'
+import LanguagePicker from './languagePicker'
 
 const reducer = (state, action) => {
   switch(action.type) {
     case('setSecretWord'):
       return { ...state, secretWord: action.payload }
+    case('setLanguage'):
+      return { ...state, language: action.payload }
     default:
       throw new Error(`Invalid action type: ${action.type}`)
   }
 }
 
 function App() {
-  const [state, dispatch] = React.useReducer(reducer, { secretWord: null })
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null, language: 'en' })
   const setSecretWord = (secretWord) => dispatch({ type: 'setSecretWord', payload: secretWord })
-
+  const setLanguage = (language) => dispatch({ type: 'setLanguage', payload: language })
+  
   React.useEffect(() => hookActions.getSecretWord(setSecretWord), [])
 
   if (!state.secretWord) {
@@ -35,7 +40,11 @@ function App() {
 
   return (
     <div className='container' data-test='component-app'>
-      <Input secretWord={state.secretWord} />
+      <h1> Jotto </h1>
+      <LanguageContext.Provider value={state.language}>
+        <LanguagePicker setLanguage={ setLanguage } />
+        <Input secretWord={state.secretWord} />
+      </LanguageContext.Provider>
     </div>
   )
 }
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import Congrats from './Congrats'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { findByTestAttr, checkProps } from '../test/testUtils'
+import languageContext from './contexts/languageContext'
 
-const defaultProps = { success: false }
-
-const setup = (props={}) => {
-  const setupProps = { ...defaultProps, ...props}
-  return shallow(<Congrats {...setupProps}/>)
+const setup = ({ success, language }) => {
+  language = language || 'en'
+  success = success || false
+  
+  return mount(
+    <languageContext.Provider value={language}>
+      <Congrats success={ success } />
+    </languageContext.Provider>
+  )
 }
 
+describe('language picker', () => {
+  test('correctly render congrats string in english', () => {
+    const wrapper = setup({ success: true })
+    
+    expect(wrapper.text()).toBe('Congratulations! You guessed the word!')
+  })
+
+  test('correctly render congrats string in emoji', () => {
+    const wrapper = setup({ success: true, language: 'emoji' })
+    
+    expect(wrapper.text()).toBe('🎯🎉')
+  })
+})
+
 test('renders without error', () => {
-  const wrapper = setup()
+  const wrapper = setup({})
   const component = findByTestAttr(wrapper, 'component-congrats')
   
   expect(component.length).toBe(1)
