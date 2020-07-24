eda218575f53dfd40fbd19c67018a3f4d581548e
Mariano Salerno <mariano.salerno@toptal.com>
2020-07-24 19:13:18 -0300

    success context with tests

---
+++ src/contexts/successContext.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import successContext from './successContext'
+
+const FunctionalComponent = () => {
+  successContext.useSuccess()
+  return <div />
+}
+
+test('useSuccess throws error when not wrapped in a SuccessProvider', () => {
+  expect(() => {
+    shallow(<FunctionalComponent />)
+  }).toThrow('useSuccess does not exist')
+})
+
+test('useSuccess does not throw error when wrapped in a SuccessProvider', () => {
+  expect(() => {
+    mount(
+      <successContext.SuccessProvider >
+        <FunctionalComponent />
+      </successContext.SuccessProvider>
+    )
+  }).not.toThrow('useSuccess does not exist')
+})
